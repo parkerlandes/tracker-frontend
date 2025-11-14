ae08b40cade4e7b183a962ddc1b4c7f786a1d325
SleepiestFool01 <teaganm856@gmail.com>
2025-11-14 10:19:15 -0600

    Fixed services & Auth

--- src/services/services.js
+++ src/services/services.js
@@ -5,9 +5,9 @@ import Router from "../router.js";
 
 var baseurl = "";
 if (import.meta.env.DEV) {
-  baseurl = "http://localhost/tracker";
+  baseurl = "http://localhost/tracker-t9";
 } else {
-  baseurl = "/tracker";
+  baseurl = "/tracker-t9";
 }
 
 const apiClient = axios.create({
