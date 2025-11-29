a08e75bd04ad3e1f35806e68ae0a6c27d94b8932
Ryan Allen <ryan.allen@eagles.oc.edu>
2025-11-28 22:41:56 -0600

    some baloney I did trying to make the login work

--- src/components/SocialLogin.vue
+++ src/components/SocialLogin.vue
@@ -39,7 +39,7 @@ const handleCredentialResponse = async (response) => {
       Utils.setStore("user", user.value);
       fName.value = user.value.fName;
       lName.value = user.value.lName;
-      
+
       if(user.value.role === "admin"){
         router.push("/admin")
       }else{
